f2f2886fe854fc0810863f59d97d3b884e873785
김세은 <139741006+seeun0210@users.noreply.github.com>
2024-01-27 19:47:52 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1580 ms, Memory: 59496 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,22 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int N=sc.nextInt();
-        int M=sc.nextInt();
-        int A[]=new int[N];
-        int S[]=new int[N+1];
-        S[0]=0;
-        for(int i=0;i<N;i++){
-            A[i]=sc.nextInt();
-            S[i+1]=A[i]+S[i];
-        }
-        for(int i=0;i<M;i++){
-            int B=sc.nextInt();
-            int C=sc.nextInt();
-            System.out.println(S[C]-S[B-1]);
-        }
-
+public static void main(String[] args) throws IOException {
+    //받는 갯수가 많을 때는 scanner보다는 BufferedReader를 사용한다.
+    BufferedReader bufferedReader
+            =new BufferedReader(new InputStreamReader(System.in));
+    //예제 첫번째 줄을 읽어옴
+    StringTokenizer stringTokenizer= new StringTokenizer(bufferedReader.readLine());
+    int suNo= Integer.parseInt(stringTokenizer.nextToken());
+    int quizNo=Integer.parseInt(stringTokenizer.nextToken());
+    long[]S=new long[suNo+1];
+    stringTokenizer= new StringTokenizer(bufferedReader.readLine());
+    for(int i=1;i<=suNo;i++){
+        S[i]=S[i-1]+Integer.parseInt(stringTokenizer.nextToken());
     }
+    for(int q=0;q<quizNo;q++){
+        stringTokenizer=new StringTokenizer(bufferedReader.readLine());
+        int i=Integer.parseInt(stringTokenizer.nextToken());
+        int j=Integer.parseInt(stringTokenizer.nextToken());
+        System.out.println(S[j]-S[i-1]);
+    }
+
+
+}
 }
